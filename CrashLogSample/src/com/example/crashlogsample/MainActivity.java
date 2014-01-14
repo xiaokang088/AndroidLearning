5d9dbd9c1bee6f23d11736c24774a911fb62d3ba
xiaokang088 <xshi@nero.com>
2014-01-14 10:19:04 +0800

    add crash log

---
+++ CrashLogSample/src/com/example/crashlogsample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.crashlogsample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements android.view.View.OnClickListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)  {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button btnSample= (Button)this.findViewById(R.id.btnSample);
+		btnSample.setOnClickListener(this); 
+		
+		ErrorReporter errReporter = new ErrorReporter();
+		errReporter.Init(this);
+		errReporter.CheckErrorAndSendMail(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	
+	public void onClick(View arg0){
+	
+		String abc= null;
+		byte[] dd = abc.getBytes();
+		
+		
+	}
+}
