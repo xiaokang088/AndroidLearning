ebd6242920973436b636de3417544d3966af4e00
xiaokang088 <xshi@nero.com>
2014-01-16 15:52:14 +0800

    add listener interface

--- FragmentSample/src/com/example/fragmentsample/HeaderFragment.java
+++ FragmentSample/src/com/example/fragmentsample/HeaderFragment.java
@@ -1,31 +1,71 @@
 package com.example.fragmentsample;
 
- 
 import android.R;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
- 
+
 public class HeaderFragment extends Fragment {
 
 	TextView tvShow;
-	
+
+	public interface ItemAddedListener {
+		public void AddItem(ItemInfo item);
+	}
+
+	private ItemAddedListener ItemAdded;
+
 	@Override
-	public  View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState){
-		View content =inflater.inflate(com.example.fragmentsample.R.layout.header_fragment, null);
-		tvShow = (TextView)content.findViewById(com.example.fragmentsample.R.id.tvShow);
-		
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View content = inflater.inflate(
+				com.example.fragmentsample.R.layout.header_fragment, null);
+		tvShow = (TextView) content
+				.findViewById(com.example.fragmentsample.R.id.tvShow);
+
+		final EditText et = (EditText) content
+				.findViewById(com.example.fragmentsample.R.id.etInput);
+		et.setOnKeyListener(new android.view.View.OnKeyListener() {
+
+			@Override
+			public boolean onKey(View arg0, int arg1, KeyEvent arg2) {
+				// TODO Auto-generated method stub
+				if (arg2.getAction() == KeyEvent.ACTION_DOWN) {
+
+					if (arg1 == KeyEvent.KEYCODE_ENTER) {
+						try {
+							ItemInfo info = new ItemInfo(et.getText().toString());
+							ItemAdded.AddItem(info);
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						}
+						et.setText("");
+						return true;
+					}
+				}
+
+				return false;
+			}
+		});
+
 		return content;
 	}
-	
+
 	@Override
-	public void onAttach(android.app.Activity activity){
+	public void onAttach(android.app.Activity activity) {
 		super.onAttach(activity);
 		
-		 
+		try{
+			ItemAdded = (ItemAddedListener)activity;
+			
+		} catch (Exception ex){
+			
+		}
 	}
 }
--- FragmentSample/src/com/example/fragmentsample/MainActivity.java
+++ FragmentSample/src/com/example/fragmentsample/MainActivity.java
@@ -5,18 +5,18 @@ import android.app.Activity;
 import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements HeaderFragment.ItemAddedListener{
+	
+	ItemAdapter adapter ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		adapter= new ItemAdapter(this,0);
 		 android.app.FragmentManager manager = this.getFragmentManager();
 		
 		 ItemFragment fragment = (ItemFragment)manager.findFragmentById(R.id.listItemFragment);
-	
-		 ItemAdapter adapter = new ItemAdapter(this,0);
+	 
 		 adapter.add(new ItemInfo("abc"));
 		 adapter.add(new ItemInfo("abdc"));
 		 adapter.add(new ItemInfo("abddc"));
@@ -29,5 +29,10 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void AddItem(ItemInfo info){
+		adapter.add(info);
+		adapter.notifyDataSetChanged();
 	}
 }
