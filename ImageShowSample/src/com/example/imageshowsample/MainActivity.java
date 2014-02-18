9dcf8cf5d480e871a170722a84c0f187969d21df
xiaokang088 <xshi@nero.com>
2014-02-18 16:49:38 +0800

    update image sample

--- ImageShowSample/src/com/example/imageshowsample/MainActivity.java
+++ ImageShowSample/src/com/example/imageshowsample/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.imageshowsample;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +23,8 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends Activity  implements android.view.View.OnClickListener{
 
+	//http://www.cnblogs.com/over140/archive/2011/06/08/2075054.html
+ 
 	ImageView imgView;
 	Button btnTest;
 	SeekBar seekBar1 ;
@@ -45,8 +58,7 @@ public class MainActivity extends Activity  implements android.view.View.OnClick
 				// TODO Auto-generated method stub
 				
 			}
-			
-			
+					
 		});
 	}
 
@@ -57,15 +69,93 @@ public class MainActivity extends Activity  implements android.view.View.OnClick
 		return true;
 	}
 	
-	int currentIndex = 0;
-	
-	
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		currentIndex++;
-		imgView.setImageResource(R.drawable.a_10 + currentIndex);
-		 
+		setImageProperty();
 	}
 
+	int currentIndex = 0;
+	void setImageTest(){
+		currentIndex++;
+		// 1.set images resource
+		// imgView.setImageResource(R.drawable.a_10 + currentIndex);
+
+		// 2. set image bitmap.
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					URL picURL = new URL("http://www.baidu.com/img/bdlogo.gif");
+					/*final Bitmap pngBM = BitmapFactory.decodeStream(picURL
+							.openStream());*/
+					final Bitmap  pngBM = BitmapFactory.decodeFile("/storage/emulated/0/Pictures/Screenshots/a.png");
+					
+					runOnUiThread(new Runnable(){
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							imgView.setImageBitmap(pngBM);
+						}
+					});
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}	 
+		}).start();
+		
+		// 3. set image uri
+		/*ContentResolver resolver = this.getContentResolver();
+		String columns[] = new String[] { MediaStore.Images.ImageColumns.DATA };
+		Cursor myCursor =  resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+				columns, null, null, null);
+		
+		myCursor.moveToFirst();
+		String url = null;
+		while(myCursor.moveToNext()){
+			  url = myCursor.getString(0);
+			  if (url!=null)
+				  break;
+		}
+		
+		File file = new File(url);
+		Uri uri = Uri.fromFile(file);*/
+		/*Uri uri = Uri.parse("/storage/emulated/0/Pictures/Screenshots/a.png");
+		imgView.setImageURI(uri);*/
+		
+		//4. set image drawable
+		/*Drawable drawable = this.getResources().getDrawable(R.drawable.a_17);
+		imgView.setImageDrawable(drawable);*/
+	}
+
+	int a=0;
+	void setImageProperty(){
+		/*1.setImageState
+		 * http://stackoverflow.com/questions/20348057/what-does-the-imageview-method-setimagestateint-state-boolean-merge-do
+		 * So it looks like if you set merge to false in setImageState(state, merge) 
+		 * then next time the view system calls onCreateDrawableState() 
+		 * it will return just the state you supplied in setImageState(state, merge).
+				If however you set merge to true in setImageState(state, merge) then
+				 next time the view system calls onCreateDrawableState() 
+				 it will return the existing state merged with the state 
+				 you supplied in setImageState(state, merge).
+				In other words, Yes the merge parameter
+				 determines whether the state you supply should be combined with 
+				 or replace the current states of the ImageView next time
+				  the view system calls onCreateDrawableState().
+
+				I still believe some explanation of the parameters 
+				should be added to the documentation. 
+				I've not come across any other public methods without any documentation so far.
+				*/
+		//imgView.setImageState(state, merge)
+		
+		imgView.setImageResource(R.drawable.weathericon);
+		
+		imgView.setImageLevel(a);
+		a= (a == 1 ? 0 : 1);
+	}
 }
