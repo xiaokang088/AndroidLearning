f613c0a0949f6f307be80c961663a1b0d977ab30
xiaokang088 <xshi@nero.com>
2014-02-20 13:21:50 +0800

    update image sample

--- ImageShowSample/src/com/example/imageshowsample/MainActivity.java
+++ ImageShowSample/src/com/example/imageshowsample/MainActivity.java
@@ -13,32 +13,35 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class MainActivity extends Activity  implements android.view.View.OnClickListener{
+public class MainActivity extends Activity implements
+		android.view.View.OnClickListener {
 
-	//http://www.cnblogs.com/over140/archive/2011/06/08/2075054.html
- 
+	// http://www.cnblogs.com/over140/archive/2011/06/08/2075054.html
+
 	ImageView imgView;
 	Button btnTest;
-	SeekBar seekBar1 ;
-	
+	SeekBar seekBar1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		imgView = (ImageView)this.findViewById(R.id.imgView);
-		btnTest = (Button)this.findViewById(R.id.btnTest);
+
+		imgView = (ImageView) this.findViewById(R.id.imgView);
+		btnTest = (Button) this.findViewById(R.id.btnTest);
 		btnTest.setOnClickListener(this);
-		seekBar1 = (SeekBar)this.findViewById(R.id.seekBar1);
-		seekBar1.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+		seekBar1 = (SeekBar) this.findViewById(R.id.seekBar1);
+		seekBar1.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
@@ -50,15 +53,15 @@ public class MainActivity extends Activity  implements android.view.View.OnClick
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-				
+
 			}
-					
+
 		});
 	}
 
@@ -68,14 +71,15 @@ public class MainActivity extends Activity  implements android.view.View.OnClick
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		setImageProperty();
 	}
 
 	int currentIndex = 0;
-	void setImageTest(){
+
+	void setImageTest() {
 		currentIndex++;
 		// 1.set images resource
 		// imgView.setImageResource(R.drawable.a_10 + currentIndex);
@@ -86,11 +90,14 @@ public class MainActivity extends Activity  implements android.view.View.OnClick
 			public void run() {
 				try {
 					URL picURL = new URL("http://www.baidu.com/img/bdlogo.gif");
-					/*final Bitmap pngBM = BitmapFactory.decodeStream(picURL
-							.openStream());*/
-					final Bitmap  pngBM = BitmapFactory.decodeFile("/storage/emulated/0/Pictures/Screenshots/a.png");
-					
-					runOnUiThread(new Runnable(){
+					/*
+					 * final Bitmap pngBM = BitmapFactory.decodeStream(picURL
+					 * .openStream());
+					 */
+					final Bitmap pngBM = BitmapFactory
+							.decodeFile("/storage/emulated/0/Pictures/Screenshots/a.png");
+
+					runOnUiThread(new Runnable() {
 						@Override
 						public void run() {
 							// TODO Auto-generated method stub
@@ -104,58 +111,83 @@ public class MainActivity extends Activity  implements android.view.View.OnClick
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			}	 
+			}
 		}).start();
-		
+
 		// 3. set image uri
-		/*ContentResolver resolver = this.getContentResolver();
-		String columns[] = new String[] { MediaStore.Images.ImageColumns.DATA };
-		Cursor myCursor =  resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-				columns, null, null, null);
-		
-		myCursor.moveToFirst();
-		String url = null;
-		while(myCursor.moveToNext()){
-			  url = myCursor.getString(0);
-			  if (url!=null)
-				  break;
-		}
-		
-		File file = new File(url);
-		Uri uri = Uri.fromFile(file);*/
-		/*Uri uri = Uri.parse("/storage/emulated/0/Pictures/Screenshots/a.png");
-		imgView.setImageURI(uri);*/
-		
-		//4. set image drawable
-		/*Drawable drawable = this.getResources().getDrawable(R.drawable.a_17);
-		imgView.setImageDrawable(drawable);*/
+		/*
+		 * ContentResolver resolver = this.getContentResolver(); String
+		 * columns[] = new String[] { MediaStore.Images.ImageColumns.DATA };
+		 * Cursor myCursor =
+		 * resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns,
+		 * null, null, null);
+		 * 
+		 * myCursor.moveToFirst(); String url = null;
+		 * while(myCursor.moveToNext()){ url = myCursor.getString(0); if
+		 * (url!=null) break; }
+		 * 
+		 * File file = new File(url); Uri uri = Uri.fromFile(file);
+		 */
+		/*
+		 * Uri uri =
+		 * Uri.parse("/storage/emulated/0/Pictures/Screenshots/a.png");
+		 * imgView.setImageURI(uri);
+		 */
+
+		// 4. set image drawable
+		/*
+		 * Drawable drawable = this.getResources().getDrawable(R.drawable.a_17);
+		 * imgView.setImageDrawable(drawable);
+		 */
 	}
 
-	int a=0;
-	void setImageProperty(){
-		/*1.setImageState
-		 * http://stackoverflow.com/questions/20348057/what-does-the-imageview-method-setimagestateint-state-boolean-merge-do
-		 * So it looks like if you set merge to false in setImageState(state, merge) 
-		 * then next time the view system calls onCreateDrawableState() 
-		 * it will return just the state you supplied in setImageState(state, merge).
-				If however you set merge to true in setImageState(state, merge) then
-				 next time the view system calls onCreateDrawableState() 
-				 it will return the existing state merged with the state 
-				 you supplied in setImageState(state, merge).
-				In other words, Yes the merge parameter
-				 determines whether the state you supply should be combined with 
-				 or replace the current states of the ImageView next time
-				  the view system calls onCreateDrawableState().
+	int a = 0;
+	int rotate = 0;
 
-				I still believe some explanation of the parameters 
-				should be added to the documentation. 
-				I've not come across any other public methods without any documentation so far.
-				*/
-		//imgView.setImageState(state, merge)
+	void setImageProperty() {
+		/*
+		 * 1.setImageState
+		 * http://stackoverflow.com/questions/20348057/what-does-
+		 * the-imageview-method-setimagestateint-state-boolean-merge-do So it
+		 * looks like if you set merge to false in setImageState(state, merge)
+		 * then next time the view system calls onCreateDrawableState() it will
+		 * return just the state you supplied in setImageState(state, merge). If
+		 * however you set merge to true in setImageState(state, merge) then
+		 * next time the view system calls onCreateDrawableState() it will
+		 * return the existing state merged with the state you supplied in
+		 * setImageState(state, merge). In other words, Yes the merge parameter
+		 * determines whether the state you supply should be combined with or
+		 * replace the current states of the ImageView next time the view system
+		 * calls onCreateDrawableState().
+		 * 
+		 * I still believe some explanation of the parameters should be added to
+		 * the documentation. I've not come across any other public methods
+		 * without any documentation so far.
+		 */
+		// imgView.setImageState(state, merge)
+
+		// 2. set ImageLevel
+		/*
+		 * imgView.setImageResource(R.drawable.weathericon);
+		 * imgView.setImageLevel(a); a = (a == 1 ? 0 : 1);
+		 */
+
+		// 3.setImageMatrix
+	    //android:scaleType="matrix"
+		 imgView.setImageResource(R.drawable.weathericon);
+		Matrix matrix = new Matrix();
+		rotate += 30;
+		if (rotate == 360) {
+			rotate = 0;
+		}
+		float centerX = imgView.getWidth() / 2;
+		float centerY = imgView.getHeight() / 2;
+		matrix.setRotate(rotate, centerX, centerY);
+		//matrix.setTranslate(10, 20);
+		imgView.setImageMatrix(matrix); 
+		//ScaleType type = ScaleType.
 		
-		imgView.setImageResource(R.drawable.weathericon);
-		
-		imgView.setImageLevel(a);
-		a= (a == 1 ? 0 : 1);
+		//imgView.setScaleType(scaleType);
+	 
 	}
 }
