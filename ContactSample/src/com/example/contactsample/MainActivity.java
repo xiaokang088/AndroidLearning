282c75d0a0ecd5851d8b4fd856d613f447a1ef14
xiaokang088 <xshi@nero.com>
2014-02-26 13:39:00 +0800

    update code for contact sample

--- ContactSample/src/com/example/contactsample/MainActivity.java
+++ ContactSample/src/com/example/contactsample/MainActivity.java
@@ -38,27 +38,7 @@ public class MainActivity extends Activity implements
 		android.view.View.OnClickListener {
 
 	String path;
-	String[] dataProjections = new String[] { ContactsContract.RawContacts.CONTACT_ID,
-			ContactsContract.RawContacts.Data.MIMETYPE,
-			ContactsContract.RawContacts.Data.DATA1,
-			ContactsContract.RawContacts.Data.DATA2,
-			ContactsContract.RawContacts.Data.DATA3,
-			ContactsContract.RawContacts.Data.DATA4,
-			ContactsContract.RawContacts.Data.DATA5,
-			ContactsContract.RawContacts.Data.DATA6,
-			ContactsContract.RawContacts.Data.DATA7,
-			ContactsContract.RawContacts.Data.DATA8,
-			ContactsContract.RawContacts.Data.DATA9,
-			ContactsContract.RawContacts.Data.DATA10,
-			ContactsContract.RawContacts.Data.DATA11,
-			ContactsContract.RawContacts.Data.DATA12,
-			ContactsContract.RawContacts.Data.DATA13,
-			ContactsContract.RawContacts.Data.DATA14,
-			ContactsContract.RawContacts.Data.DATA15,
-			};
-	// content://com.android.contacts/data
-	Uri dataUri = ContactsContract.Data.CONTENT_URI;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -107,338 +87,33 @@ public class MainActivity extends Activity implements
 		 
 	}
 
-	void getContacts() {				
+	void getContacts() {
 		ContentResolver cr = this.getContentResolver();
 		// content://com.android.contacts/raw_contacts
 		Uri rawUri = RawContacts.CONTENT_URI;
-		String[] projections = new String[] { ContactsContract.RawContacts.CONTACT_ID, ContactsContract.Data.DISPLAY_NAME };
-		Cursor rawContactCursor = cr.query(rawUri, projections, null, null, null);
-		
-		rawContactCursor.moveToFirst();	
+		String[] projections = new String[] {
+				ContactsContract.RawContacts.CONTACT_ID,
+				ContactsContract.Data.DISPLAY_NAME };
+		Cursor rawContactCursor = cr.query(rawUri, projections, null, null,
+				null);
+
+		rawContactCursor.moveToFirst();
 		List<Integer> contactids = new ArrayList<Integer>();
-		do{
+		do {
 			int id = rawContactCursor.getInt(0);
 			String name = rawContactCursor.getString(1);
 			contactids.add(id);
-		} while(rawContactCursor.moveToNext());
+		} while (rawContactCursor.moveToNext());
 		rawContactCursor.close();
-		
+
 		List<VCardStruct> VCardStructs = new ArrayList<VCardStruct>();
-		for(int contactID :contactids){			
-			VCardStruct contactStruct =  getContactSturt(contactID,cr);
+		VCardHelper helper = new VCardHelper();
+		for (int contactID : contactids) {
+			VCardStruct contactStruct = helper.getContactSturt(contactID, cr);
 			VCardStructs.add(contactStruct);
-		}		 
+		}
 		VCardHelper.WriteToVCard(VCardStructs);
 	}
 	
-	VCardStruct getContactSturt(int contactID, ContentResolver cr){		
-		VCardStruct contactStuct = new VCardStruct();
-		contactStuct.setUID(Integer.toString(contactID));
-		String selectoin = RawContacts.CONTACT_ID + " == " + contactID;
-		Cursor dataCursor = cr.query(dataUri, dataProjections, selectoin, null,
-				null);
 
-		int dataCount = dataCursor.getCount();
-		if (dataCount > 0) {
-			dataCursor.moveToFirst();
-			do {
-				String mimeType  = this.getString("mimetype", dataCursor);
-				 
-				if (mimeType.equalsIgnoreCase(Email.CONTENT_ITEM_TYPE)) {
-					parseEmail(contactStuct,dataCursor);
-					continue;
-				}
-
-				if (mimeType.equalsIgnoreCase(Phone.CONTENT_ITEM_TYPE)) {
-					parsePhone(contactStuct, dataCursor);
-					continue;
-				}
-				
-				if (mimeType.equalsIgnoreCase(StructuredName.CONTENT_ITEM_TYPE )) {
-					parseName(contactStuct,dataCursor);
-					continue;
-				}
-				
-				if (mimeType.equalsIgnoreCase(Organization.CONTENT_ITEM_TYPE )) {
-					parseOrganization(contactStuct,dataCursor);
-					continue;
-				}
-				
-				if (mimeType.equalsIgnoreCase(Website.CONTENT_ITEM_TYPE)){
-					parseWebSite(contactStuct,dataCursor);
-					continue;
-				}
-				
-				if (mimeType.equalsIgnoreCase(StructuredPostal.CONTENT_ITEM_TYPE)){
-					parseAddress(contactStuct,dataCursor);
-					continue;
-				}
-				 
-				if (mimeType.equalsIgnoreCase(Im.CONTENT_ITEM_TYPE)){
-					parseIm(contactStuct,dataCursor);
-					continue;
-				}
-				
-				if (mimeType.equalsIgnoreCase(Note.CONTENT_ITEM_TYPE)){
-					parseNote(contactStuct,dataCursor);
-					continue;
-				}
-				 
-				if (mimeType.equalsIgnoreCase(Im.CONTENT_ITEM_TYPE)){
-					parseIm(contactStuct,dataCursor);
-					continue;
-				}
-				
-			} while (dataCursor.moveToNext());
-		}
-		dataCursor.close();
-		return contactStuct;
-	}
-	
-	void parseEmail(VCardStruct contactStuct, Cursor dataCursor) {
-		/*
-		 * http://developer.android.com/reference/android/provider/ContactsContract
-		 * .CommonDataKinds.Email.html data1 String address Email address itself
-		 * data2 int Type TYPE_CUSTOM/TYPE_HOME/TYPE_WORK/TYPE_OTHER/TYPE_MOBILE
-		 */
-
-		String data1 = this.getString(ContactsContract.RawContacts.Data.DATA1,
-				dataCursor);
-		int data2 = this.getInt(ContactsContract.RawContacts.Data.DATA2,
-				dataCursor);
-		contactStuct.SetEmail(data2, data1);
-	
-	}
-
-	void parsePhone(VCardStruct contactStuct, Cursor dataCursor) {
-		
-		/*
-		http://developer.android.com/reference/android/provider/ContactsContract.CommonDataKinds.Phone.html
-		data1 	String	number		 
-		data2	int		Type		TYPE_CUSTOM. Put the actual type in LABEL.
-TYPE_HOME
-TYPE_MOBILE
-TYPE_WORK
-TYPE_FAX_WORK
-TYPE_FAX_HOME
-TYPE_PAGER
-TYPE_OTHER
-TYPE_CALLBACK
-TYPE_CAR
-TYPE_COMPANY_MAIN
-TYPE_ISDN
-TYPE_MAIN
-TYPE_OTHER_FAX
-TYPE_RADIO
-TYPE_TELEX
-TYPE_TTY_TDD
-TYPE_WORK_MOBILE
-TYPE_WORK_PAGER
-TYPE_ASSISTANT
-TYPE_MMS
-		*/
-		 
-		String data1 =  this.getString(ContactsContract.RawContacts.Data.DATA1, dataCursor);
-		int data2 = this.getInt(ContactsContract.RawContacts.Data.DATA2, dataCursor) ;
-		contactStuct.SetTel(data2, data1);
-	}
-
-	void parseName(VCardStruct contactStuct, Cursor dataCursor){
-		
-		/*String	DISPLAY_NAME	DATA1	
-		String	GIVEN_NAME	DATA2	
-		String	FAMILY_NAME	DATA3	
-		String	PREFIX	DATA4	Common prefixes in English names are "Mr", "Ms", "Dr" etc.
-		String	MIDDLE_NAME	DATA5	
-		String	SUFFIX	DATA6	Common suffixes in English names are "Sr", "Jr", "III" etc.
-		String	PHONETIC_GIVEN_NAME	DATA7	Used for phonetic spelling of the name, e.g. Pinyin, Katakana, Hiragana
-		String	PHONETIC_MIDDLE_NAME	DATA8	
-		String	PHONETIC_FAMILY_NAME	DATA9	
-		*/
-		String data1 = this.getString(ContactsContract.RawContacts.Data.DATA1, dataCursor);
-		contactStuct.SetName(data1, null);
-	}
-
-	void parseOrganization(VCardStruct contactStuct, Cursor dataCursor){
-//		String	COMPANY	DATA1	
-//		int	TYPE	DATA2	Allowed values are:
-//		TYPE_CUSTOM. Put the actual type in LABEL.
-//		TYPE_WORK
-//		TYPE_OTHER
-//		String	LABEL	DATA3	
-//		String	TITLE	DATA4	
-//		String	DEPARTMENT	DATA5	
-//		String	JOB_DESCRIPTION	DATA6	
-//		String	SYMBOL	DATA7	
-//		String	PHONETIC_NAME	DATA8	
-//		String	OFFICE_LOCATION	DATA9	
-//		String	PHONETIC_NAME_STYLE	DATA10	
-		String data1 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA1));
-		int data2 = dataCursor.getInt(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA2));
-		String data3 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA3));
-		String data4 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA4));
-		String data5 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA5));
-		String data6 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA6));
-		String data7 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA7));
-		String data8 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA8));
-		String data9 = dataCursor.getString(dataCursor
-				.getColumnIndex(Organization.OFFICE_LOCATION));
-		
-		String data10 = this.getString(Organization.PHONETIC_NAME_STYLE,
-				dataCursor);
-		
-		contactStuct.setOrg(data1 + ";" + data5);
-		contactStuct.SetTitle(data4);
-	}
-
-	void parseWebSite(VCardStruct contactStuct, Cursor dataCursor){
-//		String	URL	DATA1	
-//		int	TYPE	DATA2	Allowed values are:
-//		TYPE_CUSTOM. Put the actual type in LABEL.
-//		TYPE_HOMEPAGE
-//		TYPE_BLOG
-//		TYPE_PROFILE
-//		TYPE_HOME
-//		TYPE_WORK
-//		TYPE_FTP
-//		TYPE_OTHER
-//		String	LABEL	DATA3
-		
-		String data1 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA1));
-		int data2 = dataCursor.getInt(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA2));
-		String data3 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA3));
-		
-		contactStuct.SetUrl(data1); 
-	}
-
-	void parseAddress(VCardStruct contactStuct, Cursor dataCursor){
-		/*String	FORMATTED_ADDRESS	DATA1	
-		int	TYPE	DATA2	Allowed values are:
-		TYPE_CUSTOM. Put the actual type in LABEL.
-		TYPE_HOME
-		TYPE_WORK
-		TYPE_OTHER
-		String	LABEL	DATA3	
-		String	STREET	DATA4	
-		String	POBOX	DATA5	Post Office Box number
-		String	NEIGHBORHOOD	DATA6	
-		String	CITY	DATA7	
-		String	REGION	DATA8	
-		String	POSTCODE	DATA9	
-		String	COUNTRY	DATA10*/
-		
-	
-
-		String data1 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA1));
-		int data2 = dataCursor.getInt(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA2));
-		String data3 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA3));
-		String data4 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA4));
-		String data5 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA5));
-		String data6 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA6));
-		String data7 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA7));
-		String data8 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA8));
-		String data9 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA9));
-		String data10 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA10));
-		data4 = checkNull(data4);
-		data5 = checkNull(data5);
-		data6 =checkNull(data6);
-		data7 =checkNull(data7);
-		data8 =checkNull(data8);
-		data9 =checkNull(data9);
-		data10 =checkNull(data10);	
-		/*the post office box;
-		the extended address;
-		the street address;
-		the locality (e.g., city);
-		the region (e.g., state or province);
-		the postal code;
-		the country name
-		七个部分组成，如果，其他的一个部分没有，必须用分号分开*/
-		
-	    String address = String.format("%s;%s;%s;%s;%s;%s;%s", data5,data6,data4,data7,data8,data9,data10);    
-		contactStuct.SetAddress(data2, address);
-	}
-	
-	String checkNull(String data) {
-		return  data == null ? "" : data;
-	}
-
-	void parseIm(VCardStruct contactStuct, Cursor dataCursor){
-//		String	DATA	DATA1	
-//		int	TYPE	DATA2	Allowed values are:
-//		TYPE_CUSTOM. Put the actual type in LABEL.
-//		TYPE_HOME
-//		TYPE_WORK
-//		TYPE_OTHER
-//		String	LABEL	DATA3	
-//		String	PROTOCOL	DATA5	
-//		Allowed values:
-//
-//		PROTOCOL_CUSTOM. Also provide the actual protocol name as CUSTOM_PROTOCOL.
-//		PROTOCOL_AIM
-//		PROTOCOL_MSN
-//		PROTOCOL_YAHOO
-//		PROTOCOL_SKYPE
-//		PROTOCOL_QQ
-//		PROTOCOL_GOOGLE_TALK
-//		PROTOCOL_ICQ
-//		PROTOCOL_JABBER
-//		PROTOCOL_NETMEETING
-//		String	CUSTOM_PROTOCOL	DATA6	
-		
-		String data1 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA1));
-		int data2 = dataCursor.getInt(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA2));
-	 
-	}
-	
-	void parseNote(VCardStruct contactStuct, Cursor dataCursor){
-		String data1 = dataCursor.getString(dataCursor
-				.getColumnIndex(ContactsContract.RawContacts.Data.DATA1));
-		contactStuct.setNote(data1);
-	}
-	
-	String getString(String clumnName, Cursor dataCursor) {
-		String ret = null;
-		try {
-			ret = dataCursor.getString(dataCursor
-					.getColumnIndex(clumnName));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return ret;
-	}
-	
-	int getInt(String clumnName, Cursor dataCursor) {
-		int ret = 0;
-		try {
-			ret = dataCursor.getInt(dataCursor
-					.getColumnIndex(clumnName));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return ret;
-	}
 }
