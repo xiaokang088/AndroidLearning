03608f10659b57b91b737c0e15cb001781759b04
xiaokang088 <xshi@nero.com>
2014-03-04 16:53:39 +0800

    update layout

--- layoutSample/src/com/example/layoutsample/GravityTestActivity.java
+++ layoutSample/src/com/example/layoutsample/GravityTestActivity.java
@@ -1,25 +1,25 @@
 package com.example.layoutsample;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements
+public class GravityTestActivity extends Activity implements
 		android.view.View.OnClickListener {
 
-	
-	
+	TextView textView1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Button btn = (Button) this.findViewById(R.id.btnTest);
+		setContentView(R.layout.gravitytest);
+		Button btn = (Button) this.findViewById(R.id.btnTestGravity);
 		btn.setOnClickListener(this);
-	
+		textView1 = (TextView) this.findViewById(R.id.textView1);
 	}
 
 	@Override
@@ -29,12 +29,21 @@ public class MainActivity extends Activity implements
 		return true;
 	}
 
+	int[] GravityArr = { Gravity.TOP, Gravity.BOTTOM, Gravity.LEFT,
+			Gravity.RIGHT, Gravity.CENTER, Gravity.CENTER_HORIZONTAL,Gravity.CENTER_VERTICAL };
+
+	int i=0;
+	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		Intent intent = new Intent(this, GravityTestActivity.class);
-		this.startActivity(intent);
-
+		i++;
+		if(i>GravityArr.length-1)
+			i=0;
+		
+		int gravity = GravityArr[i];
+		textView1.setGravity(gravity);
+		textView1.setText(Integer.toString(gravity));
 	}
 
 }
