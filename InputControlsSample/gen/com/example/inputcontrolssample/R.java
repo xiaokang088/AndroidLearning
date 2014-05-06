9a6d5c6c7eacb10ad2a09151bd32b2a9c327df1e
xiaokang088 <xshi@nero.com>
2014-05-06 16:46:35 +0800

    custom shape on button

--- InputControlsSample/gen/com/example/inputcontrolssample/R.java
+++ InputControlsSample/gen/com/example/inputcontrolssample/R.java
@@ -23,31 +23,33 @@ public final class R {
     public static final class drawable {
         public static final int a=0x7f020000;
         public static final int button_custom=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int phone1=0x7f020003;
-        public static final int play_mousepress=0x7f020004;
-        public static final int play_normal=0x7f020005;
-        public static final int play_rollover=0x7f020006;
-        public static final int start_backup=0x7f020007;
-        public static final int xxx=0x7f020008;
+        public static final int button_selector=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int phone1=0x7f020004;
+        public static final int play_mousepress=0x7f020005;
+        public static final int play_normal=0x7f020006;
+        public static final int play_rollover=0x7f020007;
+        public static final int start_backup=0x7f020008;
+        public static final int xxx=0x7f020009;
     }
     public static final class id {
         public static final int Button01=0x7f080003;
-        public static final int action_settings=0x7f08000f;
+        public static final int action_settings=0x7f080010;
         public static final int btn=0x7f080002;
         public static final int button1=0x7f080001;
         public static final int button2=0x7f080004;
         public static final int button3=0x7f080005;
         public static final int button4=0x7f080006;
-        public static final int cursor=0x7f08000d;
-        public static final int expandableListView1=0x7f080008;
-        public static final int left_text=0x7f080007;
-        public static final int linearLayout1=0x7f080009;
-        public static final int text1=0x7f08000a;
-        public static final int text2=0x7f08000b;
-        public static final int text3=0x7f08000c;
+        public static final int button5=0x7f080007;
+        public static final int cursor=0x7f08000e;
+        public static final int expandableListView1=0x7f080009;
+        public static final int left_text=0x7f080008;
+        public static final int linearLayout1=0x7f08000a;
+        public static final int text1=0x7f08000b;
+        public static final int text2=0x7f08000c;
+        public static final int text3=0x7f08000d;
         public static final int textView1=0x7f080000;
-        public static final int vPager=0x7f08000e;
+        public static final int vPager=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
