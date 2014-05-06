eaa9f5495b799c8764a02d4c9fa43ccc01c62ce0
xiaokang088 <xshi@nero.com>
2014-05-06 15:49:06 +0800

    add custom dialog sample

---
+++ dialogSample/src/com/example/dialogsample/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.dialogsample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements android.view.View.OnClickListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button button1 = (Button)this.findViewById(R.id.button1);
+		button1.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		int id= v.getId();
+		
+		switch (id){
+		case R.id.button1:
+			showDialog();
+			break;
+		}
+	}
+	
+	void showDialog(){
+		
+		Dialog dialog = new Dialog(this,R.style.CustomDialogTheme);
+		View v = LayoutInflater.from(this).inflate(R.layout.dialog_test,null);
+		dialog.setContentView(v);
+		dialog.show();
+	}
+	
+
+}
---
+++ dialogSample/src/com/example/dialogsample/memo.java
@@ -0,0 +1,8 @@
+package com.example.dialogsample;
+
+public class memo {
+	/*http://stackoverflow.com/questions/12501488/android-alertdialog-with-rounded-corners
+		http://www.cnblogs.com/firecode/archive/2012/09/25/2701039.html
+			http://blog.csdn.net/dinko321/article/details/8333024
+				http://bluewhale.cc/tag/google-analytics/page/3
+*/}
