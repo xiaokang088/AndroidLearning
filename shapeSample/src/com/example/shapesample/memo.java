1ba0789cb9b95a15a7a1dc42c303fd684ff50b34
xiaokang088 <xshi@nero.com>
2014-05-07 11:00:14 +0800

    add shape sample

---
+++ shapeSample/src/com/example/shapesample/memo.java
@@ -0,0 +1,8 @@
+package com.example.shapesample;
+
+public class memo {
+/*	http://kofi1122.blog.51cto.com/2815761/521605
+		http://mzh3344258.blog.51cto.com/1823534/1215749
+			http://blog.csdn.net/ekeuy/article/details/12349853
+				http://developer.android.com/guide/topics/resources/drawable-resource.html#Shape
+*/}
